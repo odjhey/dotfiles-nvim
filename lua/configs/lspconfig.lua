28aae5f863ac74fd52b5c14de7faabae434119dd
Odelon Jhey Pacalso <odjhey@gmail.com>
2024-12-15 22:58:42 +0800

    fix tsserver as its deprecated, use ts_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "ts_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -13,10 +13,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
