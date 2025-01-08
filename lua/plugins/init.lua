98fca65bdcb918997bde87d916e1fcf6fc521840
Odelon Jhey Pacalso <odjhey@gmail.com>
2025-01-08 16:28:03 +0900

    update search todo

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -269,6 +269,20 @@ return {
       highlight = {
         pattern = [[(KEYWORDS)]], -- Match `@todo`, `todo`, and similar patterns
       },
+      search = {
+        command = "rg",
+        args = {
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+        },
+        -- regex that will be used to match keywords.
+        -- don't replace the (KEYWORDS) placeholder
+        pattern = [[(KEYWORDS)]], -- ripgrep regex
+        -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      },
     },
   },
 
